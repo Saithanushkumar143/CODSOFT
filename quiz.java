a92dbfdfd1dba572b2d55680ebff53d77171ceea
Saithanushkumar143 <166400120+Saithanushkumar143@users.noreply.github.com>
2024-05-20 20:50:59 +0530

    quiz java

--- quiz.java
+++ quiz.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
 class Quiz {
-    public static void main(String[] args) {
-    System.out.println("ENTER YOUR NAME");
-    Scanner sc = new Scanner(System.in);
-    String name = sc.next();
-    System.out.println("HI, "   + name);
-
+    public static void conductQuiz(Scanner sc) {
+    
     System.out.println("WHO WAS THE FOUNDER OF CODSPOT ?");
     System.out.println("1. STANLY WALCA");
-    System.out.println("2. ELON MUSK");
+    System.out.println("2. ELON  MUSK");
     System.out.println("3.SUNDAR PICHAY");
 
     char answer = sc.next().charAt(0);
@@ -32,6 +28,23 @@ class Quiz {
             break;
     }
 
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER YOUR NAME");
+    Scanner scanner = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println("HI, "   + name);
+
+
+
+        long startTime = System.nanoTime();
+         conductQuiz(sc);
+
+         long endTime = System.nanoTime();
+          long executionTime = (endTime - startTime)/1000000000;
+          System.out.println("Time taken: " + executionTime + "ms");
+          sc.close();
     }
 }
     
