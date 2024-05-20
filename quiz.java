c8d320c621a884720569e1ce8f85bdfe1616c879
Saithanushkumar143 <166400120+Saithanushkumar143@users.noreply.github.com>
2024-05-20 11:50:39 +0530

    QUIZ with timer.java
    This is a quiz game of multiple choice questions whichw includes the timer also

---
+++ quiz.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class Quiz {
+    public static void main(String[] args) {
+    System.out.println("ENTER YOUR NAME");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println("HI, "   + name);
+
+    System.out.println("WHO WAS THE FOUNDER OF CODSPOT ?");
+    System.out.println("1. STANLY WALCA");
+    System.out.println("2. ELON MUSK");
+    System.out.println("3.SUNDAR PICHAY");
+
+    char answer = sc.next().charAt(0);
+    switch (answer) {
+        case '1':
+        System.out.println("YES YOU ARE CORRECT DUDE");
+            
+            break;
+
+        case '2':
+        System.out.println("SORRY YOU R WRONG BRO");
+            break;
+
+        case '3':
+        System.out.println("SORRY YOU R WRONG BRO");
+            break;
+    
+        default:
+        System.out.println("USE APPORIATE OPTION");
+
+            break;
+    }
+
+    }
+}
+    
+    
